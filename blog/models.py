efcb9557bc08ed885955c1a90b17f93f88cdbef5
Simon <saintexupery@snu.ac.kr>
2016-07-25 16:22:45 +0900

    create class Post

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    photo = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
