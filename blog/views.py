e41f0afa129a52d1b847ece42bf24cb6eaa10781
Simon <saintexupery@snu.ac.kr>
2016-07-25 20:23:45 +0900

    comment에 post, user를 코드를 통해 추가

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Post, Comment
 from .forms import CommentForm
 
+@login_required
 def index(request):
     post_list = Post.objects.all()
 
@@ -10,13 +11,17 @@ def index(request):
         'post_list' : post_list,
     })
 
+@login_required
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
 
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
-            comment = form.save()
+            comment = form.save(commit=False)
+            comment.post = get_object_or_404(Post, pk=pk)
+            comment.user = request.user
+            comment.save()
             return redirect('blog:post_detail', pk)
     else:
         form = CommentForm()
