f5df5e312e00ec15243d4e148e75ea175cd820c6
Simon <saintexupery@snu.ac.kr>
2016-07-25 16:54:21 +0900

    create Comment model

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
@@ -9,3 +10,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
