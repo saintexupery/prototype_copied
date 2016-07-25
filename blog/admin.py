92cf4d1b42d36479b013f5904919026eb4efe2eb
Simon <saintexupery@snu.ac.kr>
2016-07-25 16:35:52 +0900

    add intcomma to admiin

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from django.contrib.humanize.templatetags.humanize import intcomma
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'content', 'created_at', 'updated_at']
+    list_display = ['title', 'content_size', 'created_at', 'updated_at']
+
+    def content_size(self, post):
+        return '%s자' % intcomma(len(post.content))
 
 admin.site.register(Post, PostAdmin)
